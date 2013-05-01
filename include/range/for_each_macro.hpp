22eefba89d6e862953c81595a3520717958979ab
Rogier van Dalen <rogiervd@gmail.com>
2013-05-01 21:55:24 +0100

    Introduce RANGE_FOR_EACH macro
    Ignore-this: 6cbb8ee54b4873b9900e51d8c78fea25
    
    darcs-hash:20130501205524-c9a90-ba1adbfe68d43fe157e2c6a8545e8213c831929e

---
+++ include/range/for_each_macro.hpp
@@ -0,0 +1,85 @@
+/*
+Copyright 2013 Rogier van Dalen.
+
+This file is part of Rogier van Dalen's Range library for C++.
+
+This library is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef RANGE_FOR_EACH_MACRO_HPP_INCLUDED
+#define RANGE_FOR_EACH_MACRO_HPP_INCLUDED
+
+#include "range/core.hpp"
+
+namespace range { namespace for_each_macro_detail {
+    template <class Type> struct assert_is_range {
+        static_assert (::range::is_range <Type>::value,
+            "Second parameter to RANGE_FOR_EACH must be a range");
+        typedef Type type;
+    };
+    template <class Type> typename assert_is_range <Type>::type &&
+        get_lrvalue (Type &&);
+}} // namespace range::for_each_macro_detail
+
+/** \macro RANGE_FOR_EACH
+Iterate through a range.
+Replace a normal for-statement by
+    <tt> RANGE_FOR_EACH (element, range_expression) { ... } </tt>
+
+Here, \c element is the name by which each element can be referred to inside the
+loop.
+It is of the exact type of first (range), including reference-qualification.
+
+\c range_expression is a range, which can be an expression.
+The expression is executed exactly once.
+
+\note This macro has been inspired by Boost.Foreach, though the code is much
+    simpler thanks to C++11.
+
+\internal
+This is not very readable.
+A more readable version with comments is in test/for_each_macro.cpp.
+*/
+#define RANGE_FOR_EACH(variable_name, range_expression) \
+        /* Declare variable. */ \
+        if (bool RANGE_FOR_EACH_internal_done = false) {} else \
+        /* Declare reference to range_expression. */ \
+        for (decltype ( ::range::for_each_macro_detail:: \
+                get_lrvalue (range_expression)) \
+                RANGE_FOR_EACH_internal_range = range_expression; \
+            !RANGE_FOR_EACH_internal_done; \
+            RANGE_FOR_EACH_internal_done = true) \
+        /* Declare variable. */ \
+        if (bool RANGE_FOR_EACH_internal_seen = false) {} else \
+            /* Actual for loop. */ \
+            for (auto RANGE_FOR_EACH_internal_view = \
+                    ::range::view (RANGE_FOR_EACH_internal_range); \
+                    ! ::range::empty (RANGE_FOR_EACH_internal_view); \
+                    RANGE_FOR_EACH_internal_view = \
+                        ::range::drop (RANGE_FOR_EACH_internal_view), \
+                    RANGE_FOR_EACH_internal_seen = \
+                        !RANGE_FOR_EACH_internal_seen) \
+                if (RANGE_FOR_EACH_internal_seen) \
+                    /* "break" statement inside */ \
+                    break; \
+                else \
+                    /* Declare variable "variable_name". */ \
+                    for (decltype ( ::range::first ( \
+                        RANGE_FOR_EACH_internal_view)) variable_name \
+                            = ::range::first (RANGE_FOR_EACH_internal_view); \
+                            !RANGE_FOR_EACH_internal_seen; \
+                            RANGE_FOR_EACH_internal_seen = true)
+
+#endif  // RANGE_FOR_EACH_MACRO_HPP_INCLUDED
+

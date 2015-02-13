c40c9ec116287968b217100f9e2dad3e99168060
Rogier van Dalen <rogiervd@gmail.com>
2015-02-13 18:49:22 +0000

    Introduce hash_range
    Ignore-this: edd821d2764105ceae61be6ba6dbdb66
    This computes a hash value for the whole range, taking each element into account.
    
    darcs-hash:20150213184922-c9a90-49b69911fad68a6f786835a8f9402f498ffee7a8

---
+++ include/range/hash_range.hpp
@@ -0,0 +1,114 @@
+/*
+Copyright 2015 Rogier van Dalen.
+
+This file is part of Rogier van Dalen's Range library for C++.
+
+This library is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef RANGE_HASH_RANGE_HPP_INCLUDED
+#define RANGE_HASH_RANGE_HPP_INCLUDED
+
+#include <boost/utility/enable_if.hpp>
+
+#include <boost/functional/hash_fwd.hpp>
+
+#include "core.hpp"
+#include "for_each.hpp"
+
+namespace range {
+
+namespace hash_range_detail {
+
+    class accumulate_hash {
+        std::size_t seed_;
+    public:
+        accumulate_hash (std::size_t seed) : seed_ (seed) {}
+
+        template <class Element> void operator() (Element const & e)
+        { boost::hash_combine (seed_, e); }
+
+        std::size_t seed() const { return seed_; }
+    };
+
+} // namespace hash_range_detail
+
+/** \brief
+Calculate the combined hash value of the elements of a range.
+
+boost::hash_combine is called for each element.
+boost/functional/hash.hpp must be included to use this.
+
+\param direction The direction to traverse the range in.
+\param Range The range of elements to compute the hash for.
+*/
+template <class Direction, class Range> inline
+    typename std::enable_if <is_direction <Direction>::value, std::size_t>::type
+    hash_range (Direction const & direction, Range && range)
+{
+    hash_range_detail::accumulate_hash accumulate (0);
+    range::for_each (direction, accumulate, std::forward <Range> (range));
+    return accumulate.seed();
+}
+
+/** \brief
+Calculate the combined hash value of the elements of a range.
+
+boost::hash_combine is called for each element.
+boost/functional/hash.hpp must be included to use this.
+
+The range will be traversed in horizontal direction.
+
+\param Range The range of elements to compute the hash for.
+*/
+template <class Range> inline std::size_t hash_range (Range && range)
+{ return hash_range (default_direction (range), std::forward <Range> (range)); }
+
+/** \brief
+Update the seed to include the take into account the hash value of the elements
+of the range.
+
+boost::hash_combine is called for each element.
+boost/functional/hash.hpp must be included to use this.
+
+\param direction The direction to traverse the range in.
+\param Range The range of elements to compute the hash for.
+*/
+template <class Direction, class Range> inline
+    typename std::enable_if <is_direction <Direction>::value>::type
+    hash_range (Direction const & direction, std::size_t & seed, Range && range)
+{
+    hash_range_detail::accumulate_hash accumulate (seed);
+    range::for_each (direction, accumulate, std::forward <Range> (range));
+    seed = accumulate.seed();
+}
+
+/** \brief
+Update the seed to include the take into account the hash value of the elements
+of the range.
+
+boost::hash_combine is called for each element.
+boost/functional/hash.hpp must be included to use this.
+
+The range will be traversed in horizontal direction.
+
+\param Range The range of elements to compute the hash for.
+*/
+template <class Range> inline
+    void hash_range (std::size_t & seed, Range && range)
+{ hash_range (default_direction (range), seed, std::forward <Range> (range)); }
+
+} // namespace range
+
+#endif // RANGE_HASH_RANGE_HPP_INCLUDED
---
+++ test/range/test-hash_range.cpp
@@ -0,0 +1,168 @@
+/*
+Copyright 2015 Rogier van Dalen.
+
+This file is part of Rogier van Dalen's Range library for C++.
+
+This library is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#define BOOST_TEST_MODULE test_range_hash_range
+#include "utility/test/boost_unit_test.hpp"
+
+#include "range/hash_range.hpp"
+
+#include <vector>
+#include <tuple>
+#include <string>
+
+#include <boost/functional/hash.hpp>
+
+#include "range/tuple.hpp"
+#include "range/std.hpp"
+
+using range::tuple;
+
+BOOST_AUTO_TEST_SUITE(test_range_hash_range)
+
+BOOST_AUTO_TEST_CASE (test_range_hash_homogeneous) {
+    // boost::hash_range has strictly defined behaviour so this can be used.
+    std::vector <float> v;
+
+    {
+        BOOST_CHECK_EQUAL (range::hash_range (range::front, v),
+            boost::hash_range (v.begin(), v.end()));
+        BOOST_CHECK_EQUAL (range::hash_range (v),
+            boost::hash_range (v.begin(), v.end()));
+
+        std::size_t seed = 27;
+
+        range::hash_range (range::front, seed, v);
+        BOOST_CHECK_EQUAL (seed, 27);
+
+        range::hash_range (seed, v);
+        BOOST_CHECK_EQUAL (seed, 27);
+    }
+
+    v.push_back (7.5);
+
+    {
+        BOOST_CHECK_EQUAL (range::hash_range (range::front, v),
+            boost::hash_range (v.begin(), v.end()));
+        BOOST_CHECK_EQUAL (range::hash_range (v),
+            boost::hash_range (v.begin(), v.end()));
+
+        {
+            std::size_t seed = 27;
+            std::size_t reference_seed = 27;
+
+            boost::hash_range (reference_seed, v.begin(), v.end());
+
+            range::hash_range (range::front, seed, v);
+            BOOST_CHECK_EQUAL (seed, reference_seed);
+
+            seed = 27;
+            range::hash_range (seed, v);
+            BOOST_CHECK_EQUAL (seed, reference_seed);
+        }
+    }
+
+    v.push_back (-17);
+
+    {
+        BOOST_CHECK_EQUAL (range::hash_range (range::front, v),
+            boost::hash_range (v.begin(), v.end()));
+        BOOST_CHECK_EQUAL (range::hash_range (v),
+            boost::hash_range (v.begin(), v.end()));
+
+        {
+            std::size_t seed = 27;
+            std::size_t reference_seed = 27;
+
+            boost::hash_range (reference_seed, v.begin(), v.end());
+
+            range::hash_range (range::front, seed, v);
+            BOOST_CHECK_EQUAL (seed, reference_seed);
+
+            seed = 27;
+            range::hash_range (seed, v);
+            BOOST_CHECK_EQUAL (seed, reference_seed);
+        }
+    }
+}
+
+BOOST_AUTO_TEST_CASE (test_range_hash_heterogeneous) {
+    // boost::hash_range has strictly defined behaviour so this can be used.
+    {
+        std::tuple <> t;
+
+        BOOST_CHECK_EQUAL (range::hash_range (range::front, t), 0);
+        BOOST_CHECK_EQUAL (range::hash_range (t), 0);
+    }
+
+    {
+        std::tuple <double> t (6.3);
+
+        {
+            std::size_t reference_seed = 0;
+            boost::hash_combine (reference_seed, double (6.3));
+
+            BOOST_CHECK_EQUAL (range::hash_range (range::front, t),
+                reference_seed);
+            BOOST_CHECK_EQUAL (range::hash_range (t), reference_seed);
+        }
+
+        {
+            std::size_t reference_seed = 81;
+            boost::hash_combine (reference_seed, double (6.3));
+
+            std::size_t seed = 81;
+            range::hash_range (range::front, seed, t);
+            BOOST_CHECK_EQUAL (seed, reference_seed);
+
+            seed = 81;
+            range::hash_range (seed, t);
+            BOOST_CHECK_EQUAL (seed, reference_seed);
+        }
+    }
+
+    {
+        std::tuple <double, std::string> t (10.4, "hello");
+
+        {
+            std::size_t reference_seed = 0;
+            boost::hash_combine (reference_seed, double (10.4));
+            boost::hash_combine (reference_seed, std::string ("hello"));
+
+            BOOST_CHECK_EQUAL (range::hash_range (range::front, t),
+                reference_seed);
+            BOOST_CHECK_EQUAL (range::hash_range (t), reference_seed);
+        }
+
+        {
+            std::size_t reference_seed = 81;
+            boost::hash_combine (reference_seed, double (10.4));
+            boost::hash_combine (reference_seed, std::string ("hello"));
+
+            std::size_t seed = 81;
+            range::hash_range (range::front, seed, t);
+            BOOST_CHECK_EQUAL (seed, reference_seed);
+
+            seed = 81;
+            range::hash_range (seed, t);
+            BOOST_CHECK_EQUAL (seed, reference_seed);
+        }
+    }
+}
+
+BOOST_AUTO_TEST_SUITE_END()
